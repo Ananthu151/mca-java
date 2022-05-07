6f8dd6a9fd17f3527d39fadb6dfe7001e2e36b40
Ananthu151 <93853939+Ananthu151@users.noreply.github.com>
2022-05-07 15:14:21 +0530

    Add files via upload

---
+++ CO1/Empin.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+class Empin{
+	public static void main(String[] args) {
+	Scanner sn=new Scanner(System.in);
+	int n;
+	System.out.print("Enter Number of Employees:");
+	n=sn.nextInt();
+	Teacher tr[]=new Teacher[n];
+	for (int i=0;i<n;i++) {
+		tr[i]=new Teacher();
+	}
+	for (int i=0;i<n;i++) {
+		System.out.println("******************\nDetails of Teacher"+(i+1)+":");
+		tr[i].display();
+	}
+	
+}
+}
+
+class Employee{
+	Scanner sn=new Scanner(System.in);
+	int empid;
+	String name;
+	float salary;
+	String address;
+
+	Employee(){
+		System.out.println("---------------\nEnter Details of Teacher:");
+		System.out.print("Enter Empid:");
+		empid=sn.nextInt();
+		System.out.print("Enter Name:");
+		name=sn.next();
+		System.out.print("Enter Salary:");
+		salary=sn.nextFloat();
+		System.out.print("Enter Address:");
+		address=sn.next();
+	}
+}
+class Teacher extends Employee{
+	String dept;
+	String sub;
+	Teacher(){
+		super();
+		System.out.print("Enter Department:");
+		dept=sn.next();
+		System.out.print("Enter Subject:");
+		sub=sn.next();
+	}
+
+	void display(){
+		System.out.println("Empid:"+empid);
+		System.out.println("Name:"+name);
+		System.out.println("Salary:"+salary);
+		System.out.println("Address:"+address);
+		System.out.println("Department:"+dept);
+		System.out.println("Subject:"+sub);
+	}
+}
